b39f7d3a7fa21eb22a077b2363f29115e8863335
Unknown <joeleschultz@hotmail.com>
2017-10-31 21:19:32 -0600

    10/31/2017 progress
    No errors.  No valid output.  Arrays and functions return blanks and empties.

--- euler4.py
+++ euler4.py
@@ -3,3 +3,29 @@ Euler Problem #4
 
 Find the largest palindrome which is a product of two two-digit numbers
 '''
+
+
+
+def palindrome_pairs(number):
+    ''' create list of tuples that yield palindromes '''
+    cog1 = range(1, number + 1)
+    cog2 = range(1, number + 1)
+    palindromes = []
+    for i in cog1:
+        for n in cog2:
+            answer = i * n
+            if str(answer) == reversed(str(answer)):
+                palindromes.append(i, n)
+    return palindromes
+
+def max_tuple(tuples):
+    ''' evaluate list for maximum values '''
+    ls = palindrome_pairs(tuples)
+    maximum = 0
+    for a, b in ls:
+        if a * b > maximum:
+            maximum = a * b
+    return maximum
+
+print palindrome_pairs(99)
+print max_tuple(99)
