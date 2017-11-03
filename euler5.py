069aad3011f8e51e507b465128fed8408f40f8c9
Joel Schultz <joeleschultz@hotmail.com>
2017-11-02 21:10:44 -0600

    11/2/2017 Progress
    Set factor evaluation to work backwards from maximum possible product of all factors listed.  Memory error ensues.

--- euler5.py
+++ euler5.py
@@ -1,13 +1,13 @@
 '''
 Euler Problem #5
 
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+2520 is the smallest number that can be divided by each of the numbers
+from 1 to 10 without any remainder.
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
 
 import math
-
 
 def factor(number):
     ''' Finds the factors of a given number (works)'''
@@ -18,20 +18,23 @@ def factor(number):
             factors.append(number / i)
     return factors
 
-def factor_dict(number):
-    ''' creates a dictionary of factors that are all "True".'''
-    my_dict = {}
-    for i in number:
-        my_dict[i] = True
-    return my_dict
+def sequence_multiply(range_max):
+    ''' multiplies a series of numbers together from 1 to maximum'''
+    count = range(1, range_max + 1)
+    total = 1
+    for i in count:
+        total = total * i
+    return total
+
 
 def factor_eval(max_factor):
     ''' Evaluate list for each number from 1 to 20 '''
-    factors = range(1, max_factor+1)
-    count = max_factor
-    for i in factors:
-        if count % i != 0:
-            count += 1
+    proof = range(1, max_factor+1)
+    count = sequence_multiply(max_factor)
+    test = factor(count)
+    while count != 1:
+        if sorted(proof) != sorted(test):
+            count -= 1
         else:
             return count
 
